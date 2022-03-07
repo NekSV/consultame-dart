5d508544d75e6716f1971d7862811a0e807ecbff
Alan A <alankvm@gmail.com>
2022-03-06 22:57:50 -0600

    adding no survey assigned alert

---
+++ lib/no_survey.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class NoSurvey extends StatelessWidget {
+  const NoSurvey({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Información'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text('Actualizar configuración del dispositivo')
+          ],
+        ),
+      ),
+    );
+  }
+}
